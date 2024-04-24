c8f6859929fc1b8fc02d7320f597c0440123ccac
Kilian Panot <kilian.panot@amadeus.com>
2024-04-24 14:52:00 +0900

    feat: add retry support

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { Alert, Button } from 'flowbite-svelte';
+  import { ExclamationCircleSolid } from 'flowbite-svelte-icons';
   import { EpisodesApi, SearchApi, SeasonsApi, ShowsApi } from 'sdk';
   import User from './components/User.svelte';
   import Filter from './components/Filter.svelte';
@@ -7,20 +9,25 @@
   import { page } from '$app/stores';
   import { onMount } from 'svelte';
   import { AuthenticationApi, BetaseriesIdentifier } from 'sdk';
-  import { ApiFetchClient, type ApiClient } from '@ama-sdk/core';
+  import { ApiFetchClient, ExceptionReply, type ApiClient } from '@ama-sdk/core';
   import Groups from './components/Groups.svelte';
 
   let apiClient: ApiClient | undefined;
   let userId: string | undefined;
   let filter: string = '';
+  let displayErrorBanner = false;
+
+  const retrieveToken = () => {
+    window.location.href = encodeURI(`https://www.betaseries.com/authorize?client_id=${configuration.client_id}&redirect_uri=${configuration.redirect_uri}`)
+  }
 
   onMount(() => {
     if (!$page.url.searchParams.has('code')) {
-      window.location.href = encodeURI(`https://www.betaseries.com/authorize?client_id=${configuration.client_id}&redirect_uri=${configuration.redirect_uri}`);
+      retrieveToken();
     } else {
       const code = $page.url.searchParams.get('code')!;
 
-      const betaseriesIdentifier = new BetaseriesIdentifier({...configuration, code});
+      const betaseriesIdentifier = new BetaseriesIdentifier({...configuration, code, onFailure: () => {displayErrorBanner = true;}});
       apiClient = new ApiFetchClient({requestPlugins: [betaseriesIdentifier], basePath: configuration.apiBasePath});
       const authApi = new AuthenticationApi(apiClient);
       betaseriesIdentifier.api = authApi;
@@ -37,10 +44,18 @@
 
 </script>
 
+
 {#if apiClient}
   <div class="relative px-8">
     <Filter bind:filter={filter} />
     <div class="mt-16">
+      {#if (displayErrorBanner)}
+        <Alert color="red" dismissable class="mb-2">
+          <ExclamationCircleSolid slot="icon" class="w-5 h-5" />
+          Failed to identify the user.
+          <Button slot="close-button" size="xs" on:click={() => retrieveToken()} class="ms-auto">Retry</Button>
+        </Alert>
+      {/if}
       <div class="flex flex-row">
         <div class="flex-initial shrink-0 w-0 sm:w-1/5 collapse sm:visible">
           <User apiClient={apiClient} bind:userId={userId} />
